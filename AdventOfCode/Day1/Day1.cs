1037230c22befa31536b6aef1ac1af93931e0ba5
Caleb McGraw <calebem1998@gmail.com>
2023-12-02 13:46:32 -0500

    Add project files.

---
+++ AdventOfCode/Day1/Day1.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace AdventOfCode {
+	public class Day1 {
+
+		private string input = "..\\..\\..\\Day1\\Day1Input.txt";
+
+		private int calculateValues(string line) {
+			int calculation = 0;
+
+
+			
+			return calculation;
+		}
+
+		public int Challenge() {
+			int response = 0;
+
+			var lines = File.ReadLines(input);
+			foreach (var line in lines) {
+				response += calculateValues(line);
+			}
+
+			return response;
+		}
+	}
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode {
+	public class Program {
+		public static void Main() {
+			Console.WriteLine("Choose a day: ");
+			string day = Console.ReadLine();
+
+			switch (day) {
+				case "1":
+					Day1 challenge = new Day1();
+					Console.WriteLine(challenge.Challenge());
+					break;
+
+				default:
+					Console.WriteLine("No input found");
+					break;
+			}
+		}
+	}
+}
